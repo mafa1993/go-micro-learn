c14558b828f7e85bc278818cd6b22f0a3fb2a156
Your Name <you@example.com>
2022-05-09 20:46:53 +0800

    中间件实现

--- go/src/hystrix/client/rpc/rpc.go
+++ go/src/hystrix/client/rpc/rpc.go
@@ -1,10 +1,13 @@
 package rpc
 
 import (
+	proto "client/proto"
 	"context"
 	"fmt"
-	proto "client/proto"
 
+	"client/wrapper/login"
+
+	micro "github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/client"
 )
 
@@ -12,17 +15,22 @@ var goodsCli proto.GoodsService
 
 // 所有rpc调用都需要基于这个区调用，先初始化
 func init() {
-	
+	microWra := micro.NewService(
+		micro.Name("goods.client"),                 // 定义服务名
+		micro.WrapClient(login.NewClientWrapper()), // 定义中间件
+	)
+
+	// 使用中间件重新生成rpc的客户端
+	goodsCli = proto.NewGoodsService("goods",microWra.Client()) 
 
 }
 
 // rpc调用GetGoodDetails
-func GetGoodsDetails(goodsId int) (*proto.ResponseGoods,error) {
+func GetGoodsDetails(goodsId int) (*proto.ResponseGoods, error) {
 	fmt.Println("rpc  goodsid ", goodsId)
-	goodsCli = proto.NewGoodsService("goods", client.DefaultClient)
 	res, err := goodsCli.GetGoodsDetails(context.Background(), &proto.RequestGoods{
 		GoodsId: int32(goodsId),
 	})
 
-	return res,err
+	return res, err
 }
---
+++ go/src/hystrix/client/wrapper/login/login.go
@@ -0,0 +1,33 @@
+package login
+
+import {
+	"github.com/micro/go-micro/v2/client"
+	"context"
+}
+
+
+type clientWrapper struct {
+	client.Client
+}
+
+
+// 中间件方法
+func (c *clientWrapper) Call(ctx context.Context,req client.Request,rsp interface{},opts ...client.CallOption) error {
+	//这里为中间件要执行的内容
+	fmt.Println("前置中间件执行")  
+
+	err := c.Client.Call(ctx,req,rsp,opts...)  // 这里相当于next
+
+	fmt.Println("后置中间件执行")
+
+	return err
+}
+
+// 创建中间件对象
+func NewClientWrapper() client.Wrapper {
+	return func(c client.Client) client.Client{
+		return &clientWrapper{c}
+	}
+	
+}
+
